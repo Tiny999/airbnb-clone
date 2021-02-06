6b6502fe196a4c4c79f18490dbe096be79e7dfe7
Abbas Salami <olawalesalami3@gmail.com>
2021-02-06 12:30:03 +0100

    build header component

--- src/Header.js
+++ src/Header.js
@@ -1,11 +1,27 @@
 import React from 'react';
+import logo from './logo.png';
+
+import SearchIcon from '@material-ui/icons/Search';
+import LanguageIcon from '@material-ui/icons/Language';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import { Avatar } from '@material-ui/core';
 
 import './Header.css'
 
 const Header = () => {
     return (
         <div className="header">
-            <img className='header__icon' src='https://i.pinimg.com/originals/3c/bf/3cbfbe148597341fa56f2f87ade90956.png' alt='logo'/>
+            <img className='header__icon' src={logo} alt='logo'/>
+            <div className='header__center'>
+                <input type='text'/>
+                <SearchIcon/>      
+            </div>
+            <div className='header__right'>
+                <p>Become a host</p>
+                <LanguageIcon/>
+                <ExpandMoreIcon/>
+                <Avatar/>
+            </div>
         </div>
     );
 };
