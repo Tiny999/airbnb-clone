f10a82245271d321f2a6ad1b049c79b04b6dcbe5
Abbas Salami <olawalesalami3@gmail.com>
2021-02-16 17:36:29 +0100

    finish search result

--- src/SearchPage.js
+++ src/SearchPage.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import Button from '@material-ui/core/Button';
+import SearchResult from './SearchResult';
 
 import './SearchPage.css';
 
@@ -15,6 +16,26 @@ const SearchPage = () => {
                 <Button variant="outlined">Price</Button>
                 <Button variant="outlined">Rooms and beds</Button>
                 <Button variant="outlined">More filters</Button>
+
+                
+                <SearchResult 
+                    img="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQ_wbPYTxQPMcBh7SPzLFActXnP3uhifeVT_g&usqp=CAU"
+                    location="Private room in center of London"
+                    title="Stay at this spacious Edwardian House"
+                    description="1 guest . 1 bedroom . 1 bed . 1.5 shared bathrooms . Wifi . Kitchen . Free Parking . Washing Machine"
+                    star={4.73}
+                    price="N3,000 / night"
+                    total="N12,000 total"
+                />
+                <SearchResult 
+                    img="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQ_wbPYTxQPMcBh7SPzLFActXnP3uhifeVT_g&usqp=CAU"
+                    location="Private room in center of London"
+                    title="Stay at this spacious Edwardian House"
+                    description="1 guest . 1 bedroom . 1 bed . 1.5 shared bathrooms . Wifi . Kitchen . Free Parking . Washing Machine"
+                    star={4.73}
+                    price="N3,000 / night"
+                    total="N12,000 total"
+                />
             </div>        
         </div>
     );
