205954bd5d822e388e0ed37fdd4cb4701243f72b
Abbas Salami <olawalesalami3@gmail.com>
2021-02-16 16:25:21 +0100

    start search page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
 import './App.css';
 
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+
 import Home from './Home';
+import SearchPage from './SearchPage';
 import Header from './Header';
 import Footer from './Footer';
 
 function App() {
   return (
     <div className="app">
-      <Header/>
-      <Home/>
-      <Footer/>
+      <Router>
+        <Header/>
+
+        <Switch>
+
+          <Route path='/search' component={SearchPage}/>
+          <Route path='/' component={Home}/>
+
+        </Switch>
+
+        <Footer/>
+      </Router>
     </div>
   );
 }
---
+++ src/SearchResult.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './SearchResult.css';
+
+const SearchResult = () => {
+    return (
+        <div className='searchResult'>
+            
+        </div>
+    );
+};
+
+export default SearchResult;
